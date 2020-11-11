aacfe340f69f2401b443905a6e0a162154ed0408
Aman Saraogi <72626970+SARAOGIAMAN@users.noreply.github.com>
2020-11-11 20:41:22 +0530

    Create Write a program to check whether the entered character is vowel or consonant.py

---
+++ Write a program to check whether the entered character is vowel or consonant.py
@@ -0,0 +1,7 @@
+ch = input("Enter Character : ")
+
+if(ch == 'a' or ch == 'e' or ch == 'i' or ch == 'o' or ch == 'u' or ch == 'A'
+       or ch == 'E' or ch == 'I' or ch == 'O' or ch == 'U'):
+print(ch, "is a Vowel")
+else:
+print(ch, "is a Consonant")
