2a6259f2424966ef1bfec26a56c6c00cc43656de
Aman Saraogi <72626970+SARAOGIAMAN@users.noreply.github.com>
2020-11-17 01:06:45 +0530

    Create Program to find HCF of two numbers.py

---
+++ Program to find HCF of two numbers.py
@@ -0,0 +1,12 @@
+def findhcf(x,y):
+    if x>y:
+        smaller=y
+    else:
+        smaller=x
+    for i in range(1,smaller+1):
+        if((x%i==0)and(y%i==0)):
+            hcf=i
+    return hcf
+num1=54 
+num2=24
+print("The H.C.F. is",findhcf(num1,num2))
